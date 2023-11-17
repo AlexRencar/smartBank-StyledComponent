4b3b6eb6dde3d259aae2d465773d2a51d6dcf108
Alexander Rengifo Cardona <you@example.com>
2023-11-17 17:54:15 -0500

    full project

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    parserOptions: {
+        ecmaVersion: 2022, // La versión de JavaScript que desea usar
+        sourceType: "module", // Permite el uso de "import" e "export"
+        parser: '@babel/eslint-parser',
+        requireConfigFile: false,
+    },
+};
